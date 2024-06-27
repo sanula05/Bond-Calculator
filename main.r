1fdeff05cf45f2e819f23aa5f0503a42e0636587
Sanula Nawarathna <sanula1216@gmail.com>
2024-06-27 16:45:19 +0000

    Added Yield to maturity function and a function that determines if the bond is premium or not. Added the Historical interest rates as CVS file.

--- main.r
+++ main.r
@@ -1,24 +1,57 @@
 # Parameters
-Inital_intrest_rate <- 0.08
+Cupon_rate <- 0.08
 FV <- 1000
-PMT <- Inital_intrest_rate * FV
+PMT <- Cupon_rate * FV
 Time <- 5
 # how may times a year does the bond payout
 # Payment_time_step <- 2
 # Time <- Time * Payment_time_step
-Intrest_rates <- c(0.08, 0.07, 0.07, 0.07, 0.07)
+Intrest_rates <- c(0.07, 0.07, 0.07, 0.07, 0.07)
 V <- 0
 Value <- c()
+VAM <- 1000
+
+# Build Funtion to predict the intrest rates to replace the list
+# Data from Bank of england
+Rates <- read.csv("Historical_rates.csv",header=TRUE,sep = ",")
+# print(Rates)
+
+
+
+
 
 # Price calculator,Time against inttrest rates, Time against Price 
-Price_at_maturity <- function(V, Time, PMT, Intrest_rates,FV,Inital_intrest_rate,value){
+Price_at_maturity <- function(V, Time, PMT, Intrest_rates,FV,Cupon_rate){
   for (i in 1:Time)
     if (i != Time) { 
     V <- V + PMT / (1 + Intrest_rates[i])^i
     } else {
     V <- V + (PMT + FV) / ((1+Intrest_rates[i])^i)
     }
-  return(list(V))
+  return(V)
 }
-Price_at_maturity(V, Time, PMT, Intrest_rates,FV,Inital_intrest_rate)  
-
+# Determines whather the bond is premium
+Premium <- function(PMT,FV,Cupon_rate,Time,Intrest_rates){
+  avg_rate <- mean(Intrest_rates)
+  Prm = PMT/FV
+  if (Prm > avg_rate){
+    return("Premium Bond")
+    }
+  else{
+    return("not Premium Bond")
+    }
+  }
+#Yeild to maturity
+YTM <- function(VAM,FV,Time,PMT){ 
+  poly_YTM <- c(1 + FV)
+  {for (i in 1:Time-1)
+    poly_YTM <- poly_YTM + 1}
+  poly_YTM <- (-VAM/PMT)
+  print(list(poly_YTM))
+  Solutions = polyroot(poly_YTM)
+  print(Solutions)
+  }
+Price_at_maturity(V, Time, PMT,FV,Cupon_rate)  
+Premium(PMT,FV,Cupon_rate,Time,Intrest_rates)
+YTM(VAM,FV,Time,PMT)
+# Yeild to maturity and Callable to add
