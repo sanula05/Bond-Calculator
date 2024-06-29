ec781720634b910574bf7c957c5129d17de13624
Sanula Nawarathna <sanula1216@gmail.com>
2024-06-29 00:05:51 +0000

    Created a second method of calculating YTM

--- main.r
+++ main.r
@@ -2,26 +2,21 @@
 Cupon_rate <- 0.08
 FV <- 1000
 PMT <- Cupon_rate * FV
-Time <- 5
-# how may times a year does the bond payout
-# Payment_time_step <- 2
-# Time <- Time * Payment_time_step
+Time <- 5         # In Years
 Intrest_rates <- c(0.07, 0.07, 0.07, 0.07, 0.07)
 V <- 0
 Value <- c()
-VAM <- 1000
+VAM <- 1041.002
+Solutions <- c()
 
-# Build Funtion to predict the intrest rates to replace the list
+# Funtion to predict the intrest rates to replace the list
 # Data from Bank of england
 Rates <- read.csv("Historical_rates.csv",header=TRUE,sep = ",")
-# print(Rates)
-
-
 
 
 
 # Price calculator,Time against inttrest rates, Time against Price 
-Price_at_maturity <- function(V, Time, PMT, Intrest_rates,FV,Cupon_rate){
+Price_at_maturity <- function(V, Time, PMT, Intrest_rates,FV){
   for (i in 1:Time)
     if (i != Time) { 
     V <- V + PMT / (1 + Intrest_rates[i])^i
@@ -42,16 +37,22 @@ Premium <- function(PMT,FV,Cupon_rate,Time,Intrest_rates){
     }
   }
 #Yeild to maturity
-YTM <- function(VAM,FV,Time,PMT){ 
-  poly_YTM <- c(1 + FV)
-  {for (i in 1:Time-1)
-    poly_YTM <- poly_YTM + 1}
-  poly_YTM <- (-VAM/PMT)
-  print(list(poly_YTM))
-  Solutions = polyroot(poly_YTM)
-  print(Solutions)
-  }
-Price_at_maturity(V, Time, PMT,FV,Cupon_rate)  
+YTM <- function(VAM,FV,Time,Solutions,Cupon_rate){ 
+Num_co = (1 + (FV/PMT))
+T_co = VAM / PMT
+# Solutions <- c(Solutions,Num_co)
+# for (i in 1:(Time-1)) {
+# Solutions <- c(Solutions,1)
+# }
+# Solutions <- c(Solutions,T_co)
+# print(Solutions)
+# return(polyroot(Solutions)-1)
+yeild_to_maturity <- (Cupon_rate * 1000 + (FV-VAM)/Time) / ((VAM + FV)/2)
+return(yeild_to_maturity)
+}
+
+
+
+Price_at_maturity(V, Time, PMT, Intrest_rates,FV)  
 Premium(PMT,FV,Cupon_rate,Time,Intrest_rates)
-YTM(VAM,FV,Time,PMT)
-# Yeild to maturity and Callable to add
+YTM(VAM,FV,Time,Solutions,Cupon_rate)
