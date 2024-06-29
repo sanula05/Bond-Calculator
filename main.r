264d49862cd4728134f48ca0e3989c6dbd11ff1f
Sanula Nawarathna <sanula1216@gmail.com>
2024-06-29 23:49:07 +0000

    Developed a function which calculates the Macaulay duration of a bond.

--- main.r
+++ main.r
@@ -1,12 +1,17 @@
+
+
 # Parameters
 Cupon_rate <- 0.08
+
 FV <- 1000
 PMT <- Cupon_rate * FV
-Time <- 5         # In Years
+Time <- 5       # In Years
+Current_time <- 5
 Intrest_rates <- c(0.07, 0.07, 0.07, 0.07, 0.07)
+Intrest_rate <- Intrest_rates[1]
 V <- 0
 Value <- c()
-VAM <- 1041.002
+VAM <- 1136.16
 Solutions <- c()
 
 # Funtion to predict the intrest rates to replace the list
@@ -51,8 +56,44 @@ yeild_to_maturity <- (Cupon_rate * 1000 + (FV-VAM)/Time) / ((VAM + FV)/2)
 return(yeild_to_maturity)
 }
 
+# Weighted Cashflow assuming anual payments/ number of periods = time
 
 
-Price_at_maturity(V, Time, PMT, Intrest_rates,FV)  
-Premium(PMT,FV,Cupon_rate,Time,Intrest_rates)
-YTM(VAM,FV,Time,Solutions,Cupon_rate)
+                        
+# Net Present Value Cashflow
+
+# Macaulay Duration
+Macaulay_Duration <- function(VAM,Time,Current_time,PMT,Intrest_rate,Intrest_rates,Cupon_rate,V,FV){
+  Fixed = (Time * PMT) / ((1+Intrest_rate) ^ Time)
+  Macaulay_Duration <- Fixed
+  for (i in 1:Time){
+    Macaulay_Duration <- Macaulay_Duration + (Fixed + ((i*VAM)/(1+Intrest_rate)^i)) 
+  }
+  M_Duration <- Macaulay_Duration / Price_at_maturity(V, Current_time, PMT, Intrest_rates,FV)
+  return(M_Duration)
+}
+Macaulay_Duration(VAM,Time,Current_time,PMT,Intrest_rate,Intrest_rates,Cupon_rate,V,FV)
+# Convexity
+
+# Duration Convexity 
+
+
+
+# Option <- readline(prompt <- "Value of Bond:1
+# Yeild to Maturity of Bond:2
+# Premium Bond:3");
+# Option <- as.integer(Option);
+
+# system <- function(V, Time, PMT, Intrest_rates,FV,Cupon_rate,VAM,Solutions,Option){
+#   if (Option == 1){
+Price_at_maturity(V, Time, PMT, Intrest_rates,FV) 
+#   }
+#  else if (Option == 2){
+#     YTM(VAM,FV,Time,Solutions,Cupon_rate)
+#   }
+#   else if (Option == 3){
+#     Premium(PMT,FV,Cupon_rate,Time,Intrest_rates)
+#   }
+# }
+# system(V, Time, PMT, Intrest_rates,FV,Cupon_rate,VAM,Solutions,Option)
+
